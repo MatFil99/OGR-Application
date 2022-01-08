3172cf16f393b6e7ec2c65a4d27f2f72bd5b38b6
MatFil99 <filip.mazur123@gmail.com>
2022-01-08 23:20:42 +0100

    Ogr App v1.0

--- installer-lin.sh
+++ installer-lin.sh
@@ -1,2 +1,7 @@
-call ogr-app\Scripts\activate.bat
-python ogr-app\main.py
+# install tkinter: python3-tk
+sudo apt-get install python3-tk
+
+python3 -m venv ogr-venv
+source ogr-venv/bin/activate
+pip install -r requirements.txt
+deactivate
---
+++ ogr-lin.sh
@@ -0,0 +1,2 @@
+source ogr-venv/bin/activate
+python3 ./ogr-app/main.py
